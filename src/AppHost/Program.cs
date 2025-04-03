d56fb4e20212383c61b8599d04635b9952b4398c
Ryan Cortino <ryancortino@outlook.com>
2025-04-03 14:30:50 -0500

    feat: adds redis cache and references

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-builder.AddProject<Projects.Web>("webServer");
+builder.AddProject<Projects.Web>("webServer").WithReference(cache);
 
 builder.AddProject<Projects.WindowsUI>("windowsClient");
 
